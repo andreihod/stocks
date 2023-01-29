a02fd36fe37f855f57fdcfe557df9ea1a513adbc
andreihod <andreih@outlook.com>
2023-01-29 11:30:26 -0300

    Add bottom nav bar with navigation

--- app/src/main/java/com/andreih/stocks/MainActivity.kt
+++ app/src/main/java/com/andreih/stocks/MainActivity.kt
@@ -3,21 +3,26 @@ package com.andreih.stocks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShowChart
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.andreih.stocks.commom.Result
-import com.andreih.stocks.data.model.Stock
-import com.andreih.stocks.ui.theme.StocksTheme
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.andreih.stocks.ui.screen.SearchScreen
-import com.andreih.stocks.ui.viewmodel.SearchViewModel
+import com.andreih.stocks.ui.theme.StocksTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -33,17 +38,64 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+sealed class Screen(val route: String, val text: String, val icon: ImageVector) {
+    object Stocks : Screen("stocks", "Stocks", Icons.Filled.ShowChart)
+    object Search : Screen("search", "Search", Icons.Filled.Search)
+    object Settings : Screen("settings", "Settings", Icons.Filled.Settings)
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StocksApp() {
-    val viewModel: SearchViewModel = viewModel()
-    val initialState = Result.Success(listOf<Stock>())
-    val searchStocks by viewModel.searchStocksFlow.collectAsStateWithLifecycle(initialState)
+    val navController = rememberNavController()
 
-    Surface(Modifier.fillMaxSize()) {
-        Column(Modifier.padding(16.dp)) {
-            SearchScreen(viewModel.query, searchStocks) {
-                viewModel.updateQuery(it)
+    val screens = listOf(
+        Screen.Stocks,
+        Screen.Search,
+        Screen.Settings,
+    )
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                screens.forEach { screen ->
+                    NavigationBarItem(
+                        icon = { Icon(screen.icon, contentDescription = screen.text) },
+                        label = { Text(screen.text) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                // Pop up to the start destination of the graph to
+                                // avoid building up a large stack of destinations
+                                // on the back stack as users select items
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                // Avoid multiple copies of the same destination when
+                                // reselecting the same item
+                                launchSingleTop = true
+                                // Restore state when reselecting a previously selected item
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
             }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController,
+            startDestination = Screen.Stocks.route,
+            Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+        ) {
+            composable(Screen.Stocks.route) { }
+            composable(Screen.Search.route) { SearchScreen(hiltViewModel()) }
+            composable(Screen.Settings.route) { }
         }
     }
 }
