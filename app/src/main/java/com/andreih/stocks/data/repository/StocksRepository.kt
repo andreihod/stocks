137ab2bde07666bb545d6b8d2fcb38cfd2f4bffc
andreihod <andreih@outlook.com>
2023-01-29 20:34:03 -0300

    Add DAO methods to repository

--- app/src/main/java/com/andreih/stocks/data/repository/StocksRepository.kt
+++ app/src/main/java/com/andreih/stocks/data/repository/StocksRepository.kt
@@ -2,6 +2,8 @@ package com.andreih.stocks.data.repository
 
 import com.andreih.stocks.commom.asResult
 import com.andreih.stocks.commom.Result
+import com.andreih.stocks.data.dao.StocksDao
+import com.andreih.stocks.data.entity.StockEntity
 import com.andreih.stocks.data.model.Stock
 import com.andreih.stocks.data.model.StockQuote
 import com.andreih.stocks.data.model.StockSymbol
@@ -9,17 +11,26 @@ import com.andreih.stocks.network.MarketStackNetworkDataSource
 import com.andreih.stocks.network.model.NetworkStock
 import com.andreih.stocks.network.model.intoStock
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 interface StocksRepository {
     suspend fun fetchQuote(symbol: StockSymbol): Flow<Result<StockQuote>>
 
     suspend fun search(query: String): Flow<Result<List<Stock>>>
+
+    suspend fun addSymbol(symbol: StockSymbol)
+
+    suspend fun removeSymbol(symbol: StockSymbol)
+
+    suspend fun streamSymbols(): Flow<Result<List<StockSymbol>>>
 }
 
 class StocksRepositoryImpl @Inject constructor(
-    private val marketStackNetwork: MarketStackNetworkDataSource
+    private val marketStackNetwork: MarketStackNetworkDataSource,
+    private val stocksDao: StocksDao
 ) : StocksRepository {
     override suspend fun fetchQuote(symbol: StockSymbol): Flow<Result<StockQuote>> {
         TODO()
@@ -27,4 +38,19 @@ class StocksRepositoryImpl @Inject constructor(
 
     override suspend fun search(query: String): Flow<Result<List<Stock>>> =
         flow { emit(marketStackNetwork.search(query).map(NetworkStock::intoStock)) }.asResult()
+
+    override suspend fun addSymbol(symbol: StockSymbol) {
+        stocksDao.insert(StockEntity(0, symbol.value))
+    }
+
+    override suspend fun removeSymbol(symbol: StockSymbol) {
+        stocksDao.deleteBySymbol(symbol.value)
+    }
+
+    override suspend fun streamSymbols(): Flow<Result<List<StockSymbol>>> =
+        stocksDao
+            .streamStocks()
+            .map { it.map { stock -> StockSymbol(stock.symbol) } }
+            .distinctUntilChanged()
+            .asResult()
 }
