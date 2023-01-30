630ca79dbdcb0c28fcc64038e16c475ab10ee9b7
andreihod <andreih@outlook.com>
2023-01-30 08:23:52 -0300

    Add DAO to tests

--- app/src/test/java/com/andreih/stocks/data/repository/StocksRepositoryTest.kt
+++ app/src/test/java/com/andreih/stocks/data/repository/StocksRepositoryTest.kt
@@ -2,10 +2,14 @@ package com.andreih.stocks.data.repository
 
 import com.andreih.stocks.data.model.*
 import com.andreih.stocks.commom.Result
+import com.andreih.stocks.data.dao.StocksDao
+import com.andreih.stocks.data.entity.StockEntity
 import com.andreih.stocks.network.MarketStackNetworkDataSource
 import com.andreih.stocks.network.model.NetworkStock
 import com.andreih.stocks.network.model.NetworkStockExchange
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
@@ -14,9 +18,13 @@ import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class StocksRepositoryTest {
-
     private lateinit var subjectSuccess: StocksRepository
     private lateinit var subjectError: StocksRepository
+    private val stocksDao = object : StocksDao {
+        override fun streamStocks(): Flow<List<StockEntity>> = flowOf(listOf())
+        override suspend fun insert(stock: StockEntity) {}
+        override suspend fun deleteBySymbol(stockSymbol: String) {}
+    }
 
     companion object {
         private val networkStockList = listOf(
@@ -55,17 +63,21 @@ class StocksRepositoryTest {
 
     @Before
     fun init() {
-        subjectSuccess = StocksRepositoryImpl(object : MarketStackNetworkDataSource {
-            override suspend fun search(query: String): List<NetworkStock> {
-                return networkStockList
-            }
-        })
+        subjectSuccess = StocksRepositoryImpl(
+            object : MarketStackNetworkDataSource {
+                override suspend fun search(query: String): List<NetworkStock> {
+                    return networkStockList
+                }
+            }, stocksDao
+        )
 
-        subjectError = StocksRepositoryImpl(object : MarketStackNetworkDataSource {
-            override suspend fun search(query: String): List<NetworkStock> {
-                throw throwableError
-            }
-        })
+        subjectError = StocksRepositoryImpl(
+            object : MarketStackNetworkDataSource {
+                override suspend fun search(query: String): List<NetworkStock> {
+                    throw throwableError
+                }
+            }, stocksDao
+        )
     }
 
     @Test
